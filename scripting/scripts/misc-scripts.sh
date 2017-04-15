07403eaa3415c872503083d20261e05bcaf63d38
Apollo Tang <apollotang@lights.local>
2017-04-15 15:27:23 -0400

    edit..

---
+++ scripting/scripts/misc-scripts.sh
@@ -0,0 +1,8 @@
+
+for d in part*/[0-9]*; do cp ./package.json $d/package\.json; done
+for d in mile-stones/[0-9]*/; do cp ./package.json $d/package\.json; done
+
+https://askubuntu.com/questions/300744/copy-the-content-file-to-all-subdirectory-in-a-directory-using-terminal
+
+
+-----------------------------
